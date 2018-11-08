9f0e77c1fc088c95fc87b8d8fabd4e9580121e74
Andy Sun <s.susie123@gmail.com>
2018-11-08 12:44:55 +0800

    test
    test

--- message.php
+++ message.php
@@ -1,7 +1,8 @@
 <?php
-$messagefile = fopen("message.txt", "w") or die ("Unable to send message!")
-$txt = "Name"
 
-
+$messagefile = fopen("message.txt", "w") or die ("Unable to send message!");
+$txt = "Name";
+fwrite($messagefile, $txt);
+fclose($messagefile);
 
  ?>
